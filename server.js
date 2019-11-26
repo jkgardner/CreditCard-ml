125120d320ecbf4219df516e06c7bace47cfa6ec
jkgardner <jgardn47@msudenver.edu>
2019-11-26 16:13:51 -0700

    I seem to have broken it-css & favicon not serving properly. Added some css to the home page. File upload button unclickable in the center. Cool.

--- server.js
+++ server.js
@@ -6,23 +6,25 @@ var fs = require('fs');
 var port = process.env.PORT || 1337;
 var parser = require("./parser")
 const express = require("express");
-const app = express();
-
+var app = express();
+  app.use(express.static(__dirname + 'Pages'));
 var upload = require("./processupload");
-
-
 
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
-  //var date = New Date();
   console.log("Server Started...");
   console.log(Date(Date.now().toString()));
   console.log("\r\n");
 });
 
 app.get('/home', function(req, res) {
-	console.log("GET 127.0.0.1:7000/home");
-    res.sendFile(path.join(__dirname + '/Pages/Home.html'));
+	console.log("GET /home");
+  //res.sendFile('HomePage.html', { root: path.join(__dirname, '/Pages/') });
+  res.sendFile(path.join(__dirname + '/Pages/HomePage.html'));
+});
+
+app.get('/favicon.ico', function(req, res) {
+    res.sendFile(path.join(__dirname + '/favicon.ico'));
 });
 
 app.get('/parse', function(req, res) {
